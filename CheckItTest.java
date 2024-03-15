6f19ea3e697256c2d97e2a19241e287f98b5b185
CarlaShawi <91223318+CarlaShawi@users.noreply.github.com>
2024-03-15 17:43:44 -0400

    Added few more test cases

--- CheckItTest.java
+++ CheckItTest.java
@@ -17,16 +17,34 @@ public class CheckItTest {
     @Test
     public void whenAIsFalseAndBOrCIsFalse_thenPshouldBeFalse() {
         assertFalse(CheckIt.checkIt(false, false, true));
+        assertFalse(CheckIt.checkIt(false, false, false));
+    }
+
+    @Test
+    public void whenPisFalse_thenAshouldBeFalse() {
         assertFalse(CheckIt.checkIt(false, true, false));
     }
 
     @Test
-    public void whenAIsTrueAndBAndCAreFalse_thenPshouldBeTrue() {
+    public void whenAisFalse_thenBAndCshouldNotBothBeTrue() {
+        assertTrue(CheckIt.checkIt(false, true, true));
+        assertFalse(CheckIt.checkIt(false, false, true));
+
+    }
+
+    @Test
+    public void whenAisTrue_thenPisAlwaysTrue() {
+        assertTrue(CheckIt.checkIt(true, true, true));
+        assertTrue(CheckIt.checkIt(true, true, false));
+        assertTrue(CheckIt.checkIt(true, false, true));
         assertTrue(CheckIt.checkIt(true, false, false));
     }
 
     @Test
-    public void whenAIsFalseAndBAndCAreTrue_thenPshouldBeTrue() {
-        assertTrue(CheckIt.checkIt(false, true, true));
+    public void whenPisFalse_thenEitherAisFalseOrBAndCareNotBothTrue() {
+        assertFalse(CheckIt.checkIt(false, false, true));
+        assertFalse(CheckIt.checkIt(false, true, false));
+        assertFalse(CheckIt.checkIt(false, false, false));
     }
+
 }
